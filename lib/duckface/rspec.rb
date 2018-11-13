6e4b1b95c7d523dac0df90ecb178f0b550ff753d
Michael Webb <moggyboy@gmail.com>
2018-11-13 13:08:30 +1100

    Add more useful failure message (I wasted two hours getting to the bottom of this)

--- lib/duckface/rspec.rb
+++ lib/duckface/rspec.rb
@@ -4,6 +4,9 @@ RSpec.shared_examples 'it implements' do |interface_class|
   describe "implements interface #{interface_class.name}" do
     subject(:check_it_implements) { described_class.check_it_implements(interface_class) }
 
-    it { is_expected.to be true }
+    specify do
+      custom_error_message = 'You must use `implements_interface` to enforce checking of interface'
+      expect(check_it_implements).to be(true), custom_error_message
+    end
   end
 end
